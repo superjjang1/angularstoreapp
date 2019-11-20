f4b6ed5e8fcd848ea75be536b815d9dd311ae262
superjjang1 <superjjang89@gmail.com>
2019-11-20 13:48:47 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductDetailComponent } from './product-details/product-detail.component';
 
 @NgModule({
   imports: [
