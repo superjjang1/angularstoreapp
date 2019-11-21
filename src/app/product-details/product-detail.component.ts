0ac209132ed260f3fc2b2070e1223aeacc019cab
superjjang1 <superjjang89@gmail.com>
2019-11-21 12:21:00 -0500

    2 files modified

--- src/app/product-details/product-detail.component.ts
+++ src/app/product-details/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-product-detail',
@@ -9,10 +11,16 @@ import { products } from '../products';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+    addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
   product;
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
+
   ) { }
 
 
