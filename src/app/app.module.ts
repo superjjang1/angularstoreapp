d065fb9f1fd239113adf1e1a20740178dc76f02c
superjjang1 <superjjang89@gmail.com>
2019-11-21 12:36:21 -0500

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailComponent } from './product-details/product-detail.component';
 import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -24,7 +25,8 @@ import { CartService } from './cart.service';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    CartComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
