c06ed4a499100aab743fcc11b561feaa3e6f8fe4
superjjang1 <superjjang89@gmail.com>
2019-12-01 12:33:19 -0500

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
     providedIn: 'root'
@@ -18,6 +20,8 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
 }
