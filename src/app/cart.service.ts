257d25d6040d8c1b508bb3523af8a20ccc8c6550
superjjang1 <superjjang89@gmail.com>
2019-12-01 12:34:04 -0500

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -19,6 +19,9 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
 
   constructor(
     private http: HttpClient
